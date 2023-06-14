f42606b9dabb5328e12649502cc3904202da08ae
3lthon <136120848+3lthon@users.noreply.github.com>
2023-06-13 22:15:42 -0300

    CT e-mail Invalido (Cenario de Erro) + validacao .error

--- cypress/e2e/CAC.TAT.spec.cy.js
+++ cypress/e2e/CAC.TAT.spec.cy.js
@@ -10,7 +10,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title('').should('be.equal', 'Central de Atendimento ao Cliente TAT') //Verifica o titulo da pagina visitada e valida se é igual a
       })
       //(2 e 3)
-      it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function(){
         const longText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
         cy.get('#firstName').type('Elthon')
         cy.get('#lastName').type('Coutinho')
@@ -19,7 +19,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('button[type="submit"]').click() //Elemento Inspecionado via Navegador
 
         //(4)
-        cy.get('.success').should('be.visible')
+        cy.get('.success').should('be.visible') //Verificacao da mensagem de sucesso
+
+    //Teste de Cenários alternativos
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida')
+        cy.get('#firstName').type('Elthon')
+        cy.get('#lastName').type('Coutinho')
+        cy.get('#email').type('elthon.teste-teste.com')
+        cy.get('#open-text-area').type('teste') 
+        cy.get('button[type="submit"]').click() 
+
+        cy.get('.error').should('be.visible') //verificacao da mensagem de erro
     })
       
   })
